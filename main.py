619295995ea7faa90af559f172c5ad44917b48dd
Alpha <geetanshaditya9827@gmail.com>
2024-03-14 20:57:58 +0530

    Add a decrypting feature to the code

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
-morse_code = {"a" : ".-", 
-              "b" : "-...", 
-              "c" : "-.-.", 
-              "d" : "-..", 
-              "e" : ".", 
-              "f" : "..-.", 
-              "g" : "--.", 
-              "h" : "....", 
-              "i" : "..", 
-              "j" : ".---", 
-              "k" : "-.-", 
-              "l" : ".-..", 
-              "m" : "--", 
-              "n" : "-.",
-              "o" : "---",
-              "p" : ".--.",
-              "q" : "--.-",
-              "r" : ".-.",
-              "s" : "...",
-              "t" : "-",
-              "u" : "..-",
-              "v" : "...-",
-              "w" : ".--",
-              "x" : "-..-",
-              "y" : "-.--",
-              "z" : "--..",
-              "0" : "-----",
-              "1" : ".----",
-              "2" : "..---",
-              "3" : "...--",
-              "4" : "....-",
-              "5" : ".....",
-              "6" : "-....",
-              "7" : "--...",
-              "8" : "---..",
-              "9" : "----.",
-              "." : ".-.-.-",
-              "," : "--..--",
-              "?" : "..--..",
-              "!" : "-.-.--",
-              " " : " "
-              }
+import json
+
+with open("data.json", mode="r") as file:
+    morse_code = json.load(file)
+
 
 print("Welcome to Text to Morse Code Converter")
 
-phrase = (input("Enter a word or a phrase you would like to convert to Morse code:\n")).lower()
+# Making the encrypting function
+def encrypt(text):
+    encrypted = ""
+    for i in text.lower():
+        if i == " ":
+            encrypted += "/ "
+            continue
 
-phrase_morse = ""
+        if i not in morse_code:
+            return "Invalid Characters found!"
+        encrypted += morse_code[i]
+        encrypted += " "
+    return encrypted
 
-for character in phrase:
-    for key in morse_code:
-        if character == key:
-            phrase_morse += morse_code[key]
+# Making the decryption function code
+def decrypt(text):
+    text_list = text.split()
+    print(text_list)
+    decrypted = ""
+    for i in text_list:
+        if i == "/":
+            decrypted += " "
+            continue
+        for j in morse_code:
 
-print(phrase_morse)
+            if i == morse_code[j]:
+                decrypted += j
+
+    return decrypted
+
+# Taking on User Input
+while True:
+    user = input("Would you like to encrpyt or decrypt text or Quit? Type(encrypt/decrypt/q):\n").lower()
+    if user == "encrypt":
+        encrypted_message = encrypt(input("\nType the Message...\n"))
+        print(f"\nEncrypted Message:\n{encrypted_message}\n")
+    elif user== "decrypt":
+        decrypted_message = decrypt(input("\nType the Message...\n"))
+        print(f"\nDecrypted Message:\n{decrypted_message}\n")
+    elif user == "q":
+        break
+    else:
+        print("Invalid Input")
